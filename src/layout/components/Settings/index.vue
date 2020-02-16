ba7d34f5d3e1d5f737f0e909b47c0395e6e1599e
10246 <10246346@qq.com>
2020-02-16 19:33:00 +0800

    '右侧页面'

---
+++ src/layout/components/Settings/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <p>1232131231</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Index'
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/settings.js
+++ src/settings.js
@@ -1,6 +1,15 @@
+
 module.exports = {
 
   title: 'Vue Admin Template',
+
+  // theme: variables.theme,
+
+  /**
+   * 是否显示设置右面板
+   * true(开)/false(关闭)
+   */
+  showSettings: true,
 
   /**
    * @type {boolean} true | false
